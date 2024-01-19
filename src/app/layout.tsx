20090df7e20acb4d94c3f896b5f3da635ce23166
Ianduha13 <ianduhamelhayes@gmail.com>
2024-01-19 12:05:47 -0300

    feat:adding first landing section and bringing public from example repo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  console.log(db)
   return (
     <html lang="en">
       <body className={inter.className}>

bf64343f472c1bed695a9fb1f1aa8f8b03428da4
Ianduha13 <ianduhamelhayes@gmail.com>
2024-01-19 12:50:08 -0300

    hotfix:landing mb hotfix

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -36,7 +36,7 @@ const page = () => {
         after:bg-gradient-to-l after:right-0 after:bottom-0 after:w-20 after:z-10 before:content[""] before:dark:from-brand-dark 
         before:to-transparent before:from-background before:bg-gradient-to-r before:top-0 before:left-0 before:bottom-0 before:w-20 after:absolute before:absolute 
         before:z-10
-        '>{[...Array(2)].map((arr) => <div className='flex flex-nowrap animate-slide' key={arr}>
+        '>{[...Array(2)].map((arr) => <div className='flex flex-nowrap animate-slide' key={randomUUID()}>
           {CLIENTS.map((client) => <div key={client.alt} className='relative w-[200px] m-20 shrink-0 flex items-center'>
             <Image
               src={client.logo}
@@ -272,6 +272,7 @@ const page = () => {
           ))}
         </div>
       </section>
+      <div className='h-20' />
     </>
   )
 }
